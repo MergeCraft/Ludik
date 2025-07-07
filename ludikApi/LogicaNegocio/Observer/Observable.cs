5989622fb41cab5890cef3a4a8c64d38d7c07a87
ManuelMM29 <158501393+ManuelMM29@users.noreply.github.com>
2025-07-07 11:08:27 -0300

    commit Observer

---
+++ ludikApi/LogicaNegocio/Observer/Observable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaNegocio.Observer
+{
+    public class Observable<TEvent>
+    {
+        private readonly List<IObserver<TEvent>> _observers = new();
+
+        
+        public void Subscribe(IObserver<TEvent> observer)
+        {
+            if (!_observers.Contains(observer))
+                _observers.Add(observer);
+        }
+
+        
+        public void Unsubscribe(IObserver<TEvent> observer)
+            => _observers.Remove(observer);
+
+        
+        protected void Notify(TEvent evt)
+        {
+            foreach (var obs in _observers)
+                obs.Update(evt);
+        }
+    }
+}
