3eaee0413f65055f4296d4cad336220fba947af8
Renato <96257567+RenatoRiosX@users.noreply.github.com>
2025-07-15 10:41:49 -0300

    Modificacion en clase estudiante

--- LudikAPI/LogicaNegocio/Entidades/Estudiante.cs
+++ LudikAPI/LogicaNegocio/Entidades/Estudiante.cs
@@ -22,7 +22,6 @@ namespace LogicaNegocio.Entidades
 
             foreach (var respuestaIngresada in respuestasIngresadas)
             {
-                //Todo: hay que cargar el id de la preguntaRespuestaSeguridad
                 var preguntaAlmacenada = this.PreguntasSeguridad.FirstOrDefault(p => p.Id == respuestaIngresada.Id);
 
                 if (preguntaAlmacenada == null) return false;
