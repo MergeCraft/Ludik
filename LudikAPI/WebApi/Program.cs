f49b7a8998c4ee0b033344578206ddc44eb787df
Renato <96257567+RenatoRiosX@users.noreply.github.com>
2025-07-25 11:23:19 -0300

    Se agregan medallas y tablas de equivalencia al profesor cuando se registra en el sistema

--- LudikAPI/WebApi/Program.cs
+++ LudikAPI/WebApi/Program.cs
@@ -280,6 +280,7 @@ builder.Services.AddScoped<IObtenerPreguntasDeSegurididadPorNombreUsuario, Obten
 builder.Services.AddScoped<IObtenerPreguntasDeSeguridadDelSistema, ObtenerPreguntasDeSeguridadDelSistema>();
 builder.Services.AddScoped<IObtenerSolicitudPerfilMedalla, ObtenerSolicitudesPerfilMedalla>();
 builder.Services.AddScoped<IObtenerUmbralesParaMedallasPorKudos, ObtenerUmbralesParaMedallasPorKudos>();
+builder.Services.AddScoped<IObtenerProyectoAulaColaborativo, ObtenerProyectoAulaColaborativo>();
 
 builder.Services.AddScoped<IModificarMedalla,ModificarMedalla>();
 builder.Services.AddScoped<IModificarAvatar, ModificarAvatar>();
@@ -303,6 +304,7 @@ builder.Services.AddScoped<ISeedServicio, SeedServicio>();
 builder.Services.AddScoped<IServicioDeReinicioSemanal, ServicioDeReinicioSemanal>();
 builder.Services.AddScoped<IGeneradorUrlImagen, GeneradorUrlImagen>();
 builder.Services.AddScoped<IGeneradorUrlsParaColeccionesImagenes, GeneradorUrlsParaColeccionesImagenes>();
+builder.Services.AddScoped<IServicioCrearObjetosParaProfesor, ServicioCrearObjetosParaProfesor>();
 
 
 
