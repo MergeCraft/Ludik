5debedcb6f1b5519ef00c89b9a7668b767facebb
Lucas Giusiano <luucass200026@gmail.com>
2025-08-22 18:27:16 -0300

    Update production API URL environment variable
    Replaces REACT_APP_API_URL with PRODUCTION_API_URL for production environment configuration. Ensures correct environment variable is used for API endpoint.

--- ludikclient/src/app/url.js
+++ ludikclient/src/app/url.js
@@ -3,7 +3,7 @@ let apiUrl;
 if (process.env.NODE_ENV === "development") {
   apiUrl = "https://localhost:7215";
 } else {
-  apiUrl = process.env.REACT_APP_API_URL;
+  apiUrl = process.env.PRODUCTION_API_URL;
 }
 
 export const url = apiUrl;
